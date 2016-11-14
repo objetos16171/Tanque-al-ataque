58e43b82f6bd733933b79b64e4f96c00c388a952
ubaldo16 <o.j.e.u1695@gmail.com>
2016-11-14 10:00:42 -0600

    Se creo el menu
    se agrego el menu al juego creando asi lo que sera nuestro primer nivel

---
+++ Tanque-Al -Ataque/Button.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Button here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Button extends Actor
+{
+     /**
+     * Constructor de la clase, inicializa la imágen que utiliza el objeto de esta clase.
+     */
+    public Button(GreenfootImage ima)
+    {
+        super();
+        this.setImage(ima);
+    }
+    /**
+     * Act - do whatever the Button wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
--- Tanque-Al -Ataque/TanqueWorld.java
+++ Tanque-Al -Ataque/TanqueWorld.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.LinkedList;//Crea una lista Enlazada
+import java.lang.Class;
 /**
  * Write a description of class TanqueWorld here.
  * 
@@ -14,6 +15,8 @@ public class TanqueWorld extends World
     private int i=0;
     private Tanque T1 = new Tanque();
     private Bala B = new Bala();
+    private LinkedList <GreenfootImage> imagenes;
+    private Button Start,Salir,Help;
     /**
      * Constructor for objects of class TanqueWorld.
      * 
@@ -22,14 +25,119 @@ public class TanqueWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+         imagenes = new LinkedList();
+        
+        imagenes.add(new GreenfootImage("Menu.jpg"));      //0
+        imagenes.add(new GreenfootImage("Help.png"));      //1
+        imagenes.add(new GreenfootImage("Start.png")); //2
+        imagenes.add(new GreenfootImage("Salir.png"));   //3
+        imagenes.add(new GreenfootImage("corkboard.jpg")); //4
 
+        Start = new Button(getImagen(2));
+        Help = new Button(getImagen(1));
+        Salir = new Button(getImagen(3));
+               
+        menu();
+        
+    }
+    
+    public void menu()
+    {
+        removeObjects(getObjects(null));
+        setBackground(getImagen(0));
+        addObject(Start, 200, 100);
+        addObject(Salir, 400, 100);
+        addObject(Help, 600, 100);
+        Greenfoot.setSpeed(40);
+
+    }
+    public void act()
+    {
+       
+        super.act();
+        seleccionar();
+    }
+    
+    /**
+     * Metodo encargado de crear el nivel con
+     * el fondo determinado y el actor principal
+     */
+    public void level1()
+    {
+        setBackground(getImagen(4));
         addObject(T1,390,550);
         prepare();
     }
+    
+     public void Help()
+    {
+        removeObjects(getObjects(null));
+        GreenfootImage bag = new GreenfootImage(getImagen(4));
+    }
+    
+    /**
+     *Metodo encargado de controlar los eventos de 
+     *los botones que se encuentran en la pantalla de menu 
+     *se crean los botones con los personajes para cuando sean seleccionado 
+     *cambie la imagen de nuestros personajes
+     */
+    public void seleccionar()
+    {
+        
+        if(Greenfoot.mouseClicked(Salir)) {
+            removeObjects(getObjects(null));
+            menu();
+        }
+        
+        if(Greenfoot.mouseClicked(Start)) {
+            removeObjects(getObjects(null));
+            level1();
+        }
+        
+         if(Greenfoot.mouseClicked(Help)) {
+            removeObjects(getObjects(null));
+            Help();
+        }
+    }
+    
+        /**
+     * Metodo que devolvera una imagen (tipo GreenfootImage)
+     * que contiene nuestra lista de imagenes y sera asignada
+     * a los distintos objetos del escenario.
+     * 
+     * @param int Valor de la posición de la imágen que se
+     * desea obtener.
+     * @return GreenfootImage - Imágen contenida en la Lista
+     * en la posición especificada.
+     */
+    public GreenfootImage getImagen(int n)
+    {
+        return imagenes.get(n);
+    }
+    
+    /**
+     * Este método regresa un objeto en especifico de la clase Boton.
+     * 
+     * @return Boton - Objeto jugar de la clase Boton.
+     */
+    public Button getJugar()
+    {
+        return Start;
+    }
 
+    /**
+     * Este método regresa un objeto en especifico de la clase Boton.
+     * 
+     * @return Boton - Objeto record de la clase Boton.
+     */
+    public Button getAyuda()
+    {
+        return Help;
+    }
+
+    
     public void DisparaT()
     {
-
         B.setLocation(T1.getX(),T1.getY()-100);
         addObject(B,T1.getX(),T1.getY()-100);
     }
